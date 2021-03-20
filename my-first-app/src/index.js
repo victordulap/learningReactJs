3f0f34304217daae0898ebb2262dc70a72a26f0b
Victor Dulap <anunachi1337@gmail.com>
2021-03-20 21:59:13 +0200

    now we get books from a list

--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -5,43 +5,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 // setup vars
-const firstBook = {
-  img:
-    'https://images-na.ssl-images-amazon.com/images/I/81WZ6QvGZ2L._AC_UL200_SR200,200_.jpg',
-  title: `It's Not Easy Being a Bunny (Beginner Books(R))`,
-  author: 'Marilyn Sadler',
-};
-
-const secondBook = {
-  img:
-    'https://images-na.ssl-images-amazon.com/images/I/71QuWEIWHlL._AC_UL200_SR200,200_.jpg',
-  title: `Dog Man: Mothering Heights`,
-  author: 'Dav Pilkey',
-};
+const books = [
+  {
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/81WZ6QvGZ2L._AC_UL200_SR200,200_.jpg',
+    title: `It's Not Easy Being a Bunny (Beginner Books(R))`,
+    author: 'Marilyn Sadler',
+  },
+  {
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/71QuWEIWHlL._AC_UL200_SR200,200_.jpg',
+    title: `Dog Man: Mothering Heights`,
+    author: 'Dav Pilkey',
+  },
+  {
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/81e53xsxj8L._AC_UL200_SR200,200_.jpg',
+    title: `How to Do the Work: Recognize Your Patterns, Heal from Your Past, and Create Your Self`,
+    author: 'Dr. Nicole LePera',
+  },
+];
 
 function BookList() {
   return (
     <section className="book-list">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      >
-        {/* children from props */}
-        <p>Lorem ipsum dolor sit amet.</p>
-      </Book>
+      {books.map((book) => {
+        return <Book book={book} />;
+      })}
     </section>
   );
 }
 
 const Book = (props) => {
+  console.log(props);
   // object destruction
-  const { img, title, author } = props;
+  const { img, title, author } = props.book;
 
   return (
     <article className="book">
