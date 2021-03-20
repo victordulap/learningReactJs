c603662c0187974e1d9ce7369f42ac4783d01962
Victor Dulap <anunachi1337@gmail.com>
2021-03-20 22:03:56 +0200

    added key values to books

--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -7,18 +7,21 @@ import './index.css';
 // setup vars
 const books = [
   {
+    id: 1,
     img:
       'https://images-na.ssl-images-amazon.com/images/I/81WZ6QvGZ2L._AC_UL200_SR200,200_.jpg',
     title: `It's Not Easy Being a Bunny (Beginner Books(R))`,
     author: 'Marilyn Sadler',
   },
   {
+    id: 2,
     img:
       'https://images-na.ssl-images-amazon.com/images/I/71QuWEIWHlL._AC_UL200_SR200,200_.jpg',
     title: `Dog Man: Mothering Heights`,
     author: 'Dav Pilkey',
   },
   {
+    id: 3,
     img:
       'https://images-na.ssl-images-amazon.com/images/I/81e53xsxj8L._AC_UL200_SR200,200_.jpg',
     title: `How to Do the Work: Recognize Your Patterns, Heal from Your Past, and Create Your Self`,
@@ -30,7 +33,7 @@ function BookList() {
   return (
     <section className="book-list">
       {books.map((book) => {
-        return <Book book={book} />;
+        return <Book key={book.id} book={book} />;
       })}
     </section>
   );
